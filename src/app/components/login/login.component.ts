2625891f5b4c8cafa105888d93cf3ed5a3872050
Vitor Schultz <vitorserta@gmail.com>
2024-08-03 15:27:46 -0300

    login e autenticação de usuário funcionando

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = '';
+  senha: string = '';
 
+  constructor(private router: Router, private authService: AuthService) { }
+
+  onSubmit() {
+    const user = this.authService.authenticate(this.email, this.senha);
+    if (user) {
+      if (user.role === 'Admin' || user.role === 'Docente') {
+        this.router.navigate(['/home-admin']);
+      } else if (user.role === 'Aluno') {
+        this.router.navigate(['/home-estudante']);
+      }
+    } else {
+      alert('Usuário e/ou senha incorretos');
+    }
+  }
+
+  criarConta() {
+    alert('Funcionalidade de criar conta em construção');
+  }
+
+  esqueceuSenha() {
+    alert('Funcionalidade de recuperação de senha em construção');
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,6 +4,31 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
+  private users = [
+    { email: 'admin@example.com', senha: 'admin123', role: 'Admin' },
+    { email: 'docente@example.com', senha: 'docente123', role: 'Docente' },
+    { email: 'aluno@example.com', senha: 'aluno123', role: 'Aluno' }
+  ];
 
   constructor() { }
+
+  authenticate(email: string, senha: string) {
+    const user = this.users.find(user => user.email === email && user.senha === senha);
+    if (user) {
+      localStorage.setItem('usuarioLogado', JSON.stringify(user));
+    }
+    return user;
+  }
+
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem('usuarioLogado');
+  }
+
+  getUsuarioLogado() {
+    return JSON.parse(localStorage.getItem('usuarioLogado') || '{}');
+  }
+
+  logout() {
+    localStorage.removeItem('usuarioLogado');
+  }
 }
